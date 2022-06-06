c47da2b4fc4e1af431fa513fc9ee46d40129ae2a
Lovepreet383 <106957710+Lovepreet383@users.noreply.github.com>
2022-06-06 05:09:01 -0400

    Add files via upload
    Initial commit

---
+++ car.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+import sklearn
+import pickle
+from pathlib import Path
+app = Flask(__name__)
+
+model = pickle.load(open("LinearRegressionModel.pkl", 'rb'))
+car = pd.read_csv("Cleaned_car_data.csv")
+
+@app.route('/')
+def index():
+    companies = sorted(car["company"].unique())
+    car_models = sorted(car["name"].unique())
+    year = sorted(car["year"].unique(), reverse= True)
+    fuel_type = car["fuel_type"].unique()
+    companies.insert(0, "Select Company")
+    return render_template("index.html", companies = companies, car_models = car_models,
+                           years = year, fuel_types = fuel_type)
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    company = request.form.get("company")
+    car_model = request.form.get("car_model")
+    year = request.form.get("year")
+    fuel_type = request.form.get("fuel_type")
+    kms_driven = int(request.form.get("kms_driven"))
+
+    prediction = model.predict(pd.DataFrame([[car_model, company, year, fuel_type, kms_driven]], columns = ['name', 'company', 'year', 'fuel_type', 'kms_driven']))
+
+    return str(np.round(prediction[0][0], 2))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
